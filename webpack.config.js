4bed23ea6a541ff40e9d82e30ca7b433166c2495
andrew <andrewlamansky@gmail.com>
2021-01-16 15:59:03 -0500

    webpack config reads .env file for input/output dirs

--- webpack.config.js
+++ webpack.config.js
@@ -14,38 +14,43 @@ const getJavascriptRegex = (usesTypescript) => {
 };
 
 const config = {
+	entry_dir: process.env.ENTRY_DIR,
+	output_dir: process.env.OUTPUT_DIR,
 	js_framework: process.env.JS_FRAMEWORK,
 	js_superset: process.env.TYPESCRIPT,
 	js_ext_regex: getJavascriptRegex(process.env.TYPESCRIPT),
 	js_ext: process.env.JS_EXT,
+	css_webpack: process.env.CSS_WEBPACK,
+	html_webpack: process.env.HTML_WEBPACK,
 	css_pre: process.env.CSS_PREPROCESSOR,
 	css_pre_ext: RegExp(`.${process.env.CSS_PREPROCESSOR_EXT}$`),
 };
 
 module.exports = {
 	mode: 'development',
-	entry: `./src/index.${config.js_ext}`,
+	entry: `./${config.entry_dir}/index.${config.js_ext}`,
 	output: {
-		path: path.resolve(__dirname, 'dist'), //default filename
+		path: path.resolve(__dirname, `${config.output_dir}`), //default filename
 		filename: 'main.js', //default path
 	},
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
 	},
 	module: {
-		rules: [
-			{ test: config.js_ext_regex, exclude: /node_modules/, use: javascriptLoaderConfig(config.js_superset) },
-			{
-				test: config.css_pre_ext,
-				use: styleLoaderConfig(config.css_pre),
-			},
-		],
+		rules: []
+			.concat({ test: config.js_ext_regex, exclude: /node_modules/, use: javascriptLoaderConfig(config.js_superset) })
+			.concat(config.css_webpack ? { test: config.css_pre_ext, use: styleLoaderConfig(config.css_pre) } : null)
+			.filter((x) => x),
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			filename: './index.html',
-		}),
-	],
+	plugins: []
+		.concat(
+			config.html_webpack
+				? new HtmlWebpackPlugin({
+						template: './src/index.html',
+						filename: './index.html',
+				  })
+				: null,
+		)
+		.filter((x) => x),
 	devtool: 'source-map', //change for production
 };
